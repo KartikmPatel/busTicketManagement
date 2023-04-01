e9d20be10c8541cd1c8786f929b4b6a000f3a505
naitik patel <pnaitik504@gmail.com>
2023-04-01 19:49:17 +0530

    footer

--- resources/views/Layouts/footer.blade.php
+++ resources/views/Layouts/footer.blade.php
@@ -1,1 +1,15 @@
-<h2> footer </h2>
+<p class="footer">
+    Bus Management System © 2023
+</p>
+<style>
+    .footer{
+        background-color: rgb(26, 86, 177);
+        width: 100%;
+        height: 40px;
+        /* flood-color: chartreuse; */
+        color: rgb(79, 207, 230);
+        text-align: center;
+        bottom: 0px;
+        position: absolute;
+    }
+</style>
