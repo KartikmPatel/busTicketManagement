65192d36b0058332d5fe136015e281082ba5cb9c
naitik patel <pnaitik504@gmail.com>
2023-04-02 21:39:28 +0530

    aside color

--- resources/views/Layouts/footer.blade.php
+++ resources/views/Layouts/footer.blade.php
@@ -5,11 +5,12 @@
     .footer{
         background-color: rgb(26, 86, 177);
         width: 100%;
-        height: 40px;
+        height: 30px;
         /* flood-color: chartreuse; */
         color: rgb(79, 207, 230);
         text-align: center;
         bottom: 0px;
         position: absolute;
+        padding-bottom: 0px;
     }
 </style>
