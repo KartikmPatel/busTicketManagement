01dae39df50ebe860b492a6bee5fc643bb8e7495
KartikmPatel <kartikmpatel0804@gmail.com>
2023-06-07 11:24:01 +0530

    ChangeDate

--- resources/views/User/searchBus.blade.php
+++ resources/views/User/searchBus.blade.php
@@ -2,23 +2,28 @@
 
 @section('Usermain-content')
 <body class="bg-white" onload="myFunction()">
+    <div class="row next">
+    <i class="fa-solid fa-angle-left" id="n1"></i>
+    <span id="date1"></span>
+    <i class="fa-solid fa-angle-right" id="n1" onclick="nextDate()"></i>
+    </div>
     <div class="viewBus">
     <table class="content-table col-md-7">
   <thead>
     <tr>
-      <th></th>
-      <th> {{__('home.bno')}} </th>
+        <th></th>
+        <th> {{__('home.bno')}} </th>
       <th> {{__('home.type')}} </th>
       <th> {{__('home.dTime')}} </th>
       <th> {{__('home.fare')}} </th>
       <th> {{__('home.vSeat')}} <th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach ($routes as $route)
-                <tr>
-                    <td>
-                        <img src="./images1/bus13.jpg" width="70px" height="50px" alt=""/>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($routes as $route)
+            <tr>
+                <td>
+                    <img src="./images1/bus13.jpg" width="70px" height="50px" alt=""/>
                     </td>
                     <td class="bno">
                         {{ $route->busNo }}
@@ -27,8 +32,8 @@
                     @foreach ($buses as $bus)
                     @if ($bus->busNo == $route->busNo)
                         {{ $bus->type }}
-                    @endif
-                    @endforeach
+                        @endif
+                        @endforeach
                     </td>
                     <td class="departureTime col-md-3">
                         {{ $route->departureTime }}
@@ -45,15 +50,20 @@
                             <input type="hidden" name="date" id="date" value="{{$date}}"/>
                             <input type="hidden" name="time" id="time" value="{{$route->departureTime}}"/>
                             <input type="hidden" name="fare" id="fare" value="{{$route->fare}}"/>
-
+                            
                             <input type="hidden" name="bno" id="bno" value="{{$route->busNo}}"/>
-                        <button class='btn btn-warning btn-sm'><i class="fa fa-eye" aria-hidden="true"></i></button>
+                            <button class='btn btn-warning btn-sm'><i class="fa fa-eye" aria-hidden="true"></i></button>
                         </form>
                     </td>
                 </tr>
-            @endforeach
-  </tbody>
+                @endforeach
+            </tbody>
 </table>
     </div>
+
+    <script>        
+        var date = document.getElementById("date").value;
+        document.getElementById("date1").innerHTML = date;
+    </script>
 </body>
 @endsection
